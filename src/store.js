acf67c9a991d73671ed8eff82848b6c4d585f687
Naime Ahmed <naime1947@gmail.com>
2020-06-13 14:24:44 +0600

    Redux persist incorporate for using browser local storage

--- src/store.js
+++ src/store.js
@@ -1,7 +1,16 @@
 import {createStore, combineReducers} from 'redux';
 import {todos} from './components/reducers';
+import {persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
 
 const reducers = {todos};
+const persistConfig = {
+    key: 'root',
+    storage,
+    stateReconciler: autoMergeLevel2
+}
 
-const rootReducers = combineReducers(reducers);
-export const configureStore  = () => createStore(rootReducers);
+const rootReducer = combineReducers(reducers);
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+export const configureStore  = () => createStore(persistedReducer, window.__REDUX_DEVTOOLS_EXTENSION__&&window.__REDUX_DEVTOOLS_EXTENSION__());
